40ca857fae928a66478ad2d7dc941448369c10cc
Rono-Koushique <rono2170@gmail.com>
2023-06-20 00:30:43 +0600

    finalized module 3 assignment

--- module 3/assignment/script.js
+++ module 3/assignment/script.js
@@ -24,15 +24,11 @@ function getDayFromDate(dateString) {
 /*                                  problem 2                                 */
 /* -------------------------------------------------------------------------- */
 function rootSum(arr) {
-    try {
-        const sum = arr.reduce((total, element) => {
-            return total + Math.pow(element, 2);
-        }, 0);
-        const root = Math.sqrt(sum);
-        return root;
-    } catch (error) {
-        return;
-    }
+    const sum = arr.reduce((total, element) => {
+        return total + Math.pow(element, 2);
+    }, 0);
+    const root = Math.sqrt(sum);
+    return root;
 }
 
 /* -------------------------------------------------------------------------- */
@@ -49,7 +45,9 @@ function isPrime(number) {
 /* -------------------------------------------------------------------------- */
 /*                                  problem 4                                 */
 /* -------------------------------------------------------------------------- */
-
+function gotoExample(url, width, height) {
+    window.open(url, "_blank", `width=${width},height=${height}`);
+}
 
 /* -------------------------------------------------------------------------- */
 /*                                  problem 5                                 */
@@ -60,6 +58,8 @@ function getBrowserInfo() {
     let browerVersion;
     let versionRegex;
 
+    // getting browser name and
+    // setting corresponding regex for getting version
     if (userAgent.indexOf("Firefox") > -1) {
         browserName = "Mozilla Firefox";
         versionRegex = /Firefox\/([\d.]+)/;
@@ -85,6 +85,8 @@ function getBrowserInfo() {
         browserName = "Unknown";
         versionRegex = /(?:\b(?:Version|MSIE|rv)[: ]|[^a-z]Edge\/)([\d.]+)/;
     }
+
+    // getting browser version
     let matches = userAgent.match(versionRegex);
     browerVersion = matches ? matches[1] : "Unknown";
     return `${browserName} v${browerVersion}`;
@@ -93,27 +95,138 @@ function getBrowserInfo() {
 /* -------------------------------------------------------------------------- */
 /*                                  problem 6                                 */
 /* -------------------------------------------------------------------------- */
-
-function getUserLocation() {
-    const locationStatus = document.getElementById("location-status");
-    const locationOutput = document.getElementById("location-output");
-
-    function success(position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        locationStatus.innerText = "";
-        locationOutput.innerText = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-    }
-
-    function error() {
-        locationStatus.innerText = "Unable to retrieve your location";
-    }
-
-    if (!navigator.geolocation) {
-        locationStatus.innerText =
-            "Geolocation is not supported by your browser";
+function getUserLocation(element = undefined) {
+    let locationStatus;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                const latitudeText = latitude.toFixed(4);
+                const longitudeText = longitude.toFixed(4);
+                locationStatus = `Latitude: ${latitudeText}°<br> Longitude: ${longitudeText}°`;
+                if (element) {
+                    element.innerHTML = locationStatus;
+                } else {
+                    console.log(locationStatus);
+                }
+            },
+            () => {
+                locationStatus = "Unable to retrieve your location";
+                if (element) {
+                    element.innerHTML = locationStatus;
+                } else {
+                    console.log(locationStatus);
+                }
+            }
+        );
     } else {
-        locationStatus.innerText = "Locating...";
-        navigator.geolocation.getCurrentPosition(success, error);
+        console.log(error);
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                  problem 7                                 */
+/* -------------------------------------------------------------------------- */
+// inside webpage
+
+/* -------------------------------------------------------------------------- */
+/*                                  problem 8                                 */
+/* -------------------------------------------------------------------------- */
+function showText(element) {
+    element.innerText = "Button Clicked!";
+}
+
+function removeText(element) {
+    element.innerText = "";
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                 problem 10                                 */
+/* -------------------------------------------------------------------------- */
+function changeTextToYellow(element) {
+    if (!element.classList.contains("hightlight")) {
+        element.classList.add("highlight");
+    }
+}
+
+function resetTextFromYellow(element) {
+    element.classList.remove("highlight");
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                 problem 11                                 */
+/* -------------------------------------------------------------------------- */
+function addItemsToList(list) {
+    const listItems = list.childNodes;
+    const newItem = document.createElement("li");
+    newItem.classList.add("my-li");
+
+    const idx = listItems.length + 1;
+    newItem.innerText = idx;
+    if (listItems.length < 5) {
+        list.appendChild(newItem);
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                 problem 12                                 */
+/* -------------------------------------------------------------------------- */
+function switchImage(imgElement) {
+    const imageList = ["p0jz4pmnl9E", "h_XcnLbDc_0", "qbOnG07yDCM"];
+    currentImageIndex = currentImageIndex + 1;
+    if (currentImageIndex % imageList.length === 0) currentImageIndex = 0;
+    currentImageUrl = `https://source.unsplash.com/${imageList[currentImageIndex]}`;
+    imgElement.src = currentImageUrl;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                 problem 13                                 */
+/* -------------------------------------------------------------------------- */
+function changeTextColor(event) {
+    // reseting all paragraphs
+    const allParagraphs = document.querySelectorAll("p");
+    allParagraphs.forEach((element) => {
+        element.classList.remove("red");
+    });
+
+    // targetting selected paragraphs
+    const className = event.target.value;
+    if (className) {
+        const targetParagraphs = document.querySelectorAll(`.${className}`);
+        targetParagraphs.forEach((element) => {
+            element.classList.add("red");
+        });
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                 problem 14                                 */
+/* -------------------------------------------------------------------------- */
+async function sendRequest() {
+    try {
+        let res = await axios.get("https://jsonplaceholder.typicode.com/users");
+        console.log(res.data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                  prblem 15                                 */
+/* -------------------------------------------------------------------------- */
+async function sendRequest() {
+    const jsonObj = {
+        name: "John Doe",
+        email: "johndoe@example.com",
+    };
+    try {
+        let res = await axios.post(
+            "https://jsonplaceholder.typicode.com/users",
+            jsonObj
+        );
+        console.log(res.data);
+    } catch (error) {
+        console.log(error);
     }
 }
